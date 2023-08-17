61d6cf4c224c76c7cd6b7d69580e276aafd23638
ShEm5 <shoshtaj8@gmail.com>
2023-08-17 02:32:10 -0700

    “c”

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print nubers - print 2-4
+ use _putchar twice
+ * Return: Always 0.
+ */
+int main(void)
+{
+	if num;
+	for (num <= 9 || num = 0; num++)
+	{
+		if (num == 2 || num == 4)
+			continue;
+		_putchar(num + 48);
+	}
+	_putchar('\n');
